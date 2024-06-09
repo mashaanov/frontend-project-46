6621dd691daa2fd86a29a6f2545111a3f1759afa
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-09 15:03:18 +0400

    update  gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,9 @@
 import { program } from 'commander';
 
 program
-  .description("Compares two configuration files and shows a difference.")
-  .option("-V, --version", "output the version number")
-  .helpOption("-h, --help", "output usage information");
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information');
 program.parse(process.argv);

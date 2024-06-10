d13e54dc38f64f5b5d10044bd0b4c92dd700f76b
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-11 03:55:05 +0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import path from 'path';
 import fs from 'fs';
 import parse from './parse.js';
 import buildTree from './buildTree.js';
+import diffFormatter from './diffFormatter.js';
 
 const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const getExtFormat = (filepath) => path.extname(filepath).slice(1);
 
 const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
 
-const getData = (filepath) => parse(fullFilePath1, getExtFormat(filepath));
+const getData = (filepath) => parse(filepath, getExtFormat(filepath));
 
 const genDiff = (filepath1, filepath2) => {
   const fullFilePath1 = readFile(getFullPath(filepath1));
@@ -16,6 +17,6 @@ const genDiff = (filepath1, filepath2) => {
 
   const data1 = getData(fullFilePath1);
   const data2 = getData(fullFilePath2);
-  console.log(data1, data2, buildTree(data1, data2));
+  return diffFormatter(buildTree(data1, data2));
 };
 export default genDiff;

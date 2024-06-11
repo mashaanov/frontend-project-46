af613f161089d68d50e9deabd00fb54404338ef6
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-11 21:38:03 +0400

    update helper.js

---
+++ src/helper.js
@@ -0,0 +1,8 @@
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+export default getFixturePath;

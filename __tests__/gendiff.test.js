2cccdefacb92f02ad57a3d1e86fbcb0fc9f53df5
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-12 13:01:51 +0400

    update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,8 +6,8 @@ test('genDiff should return correct diff', () => {
     const filePath1 = getFixturePath('file1.json');
     const filePath2 = getFixturePath('file2.json');
     const resultPath = getFixturePath('result_genDiff.txt');
-  
+
     const expectedResult = readFile(resultPath).trim(); // Удаление лишних пробелов и переносов строк
-  
+
     expect(genDiff(filePath1, filePath2)).toEqual(expectedResult);
-  });
+});

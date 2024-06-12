bb895536ff6b626b6c3b8bc21e9b57e6dede2397
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-12 12:20:50 +0400

    update buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -3,21 +3,21 @@ import _ from 'lodash';
 const buildTree = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
-  const keys = _.sortedUniqBy(_.union(keys1, keys2));
+  const keys = _.sortBy(_.union(keys1, keys2));
 
-  keys.map((key) => {
-    if (!Object.hasOwn(keys1, key)) {
+  const diffTree = keys.map((key) => {
+    if (!Object.hasOwn(data1, key)) {
       return { key, type: 'added', value: data2[key] };
-    }
-    if (!Object.hasOwn(keys2, key)) {
+    } if (!Object.hasOwn(data2, key)) {
       return { key, type: 'deleted', value: data1[key] };
-    }
-    if (data1[key] !== data2[key]) {
+    } if (data1[key] !== data2[key]) {
       return {
         key, type: 'changed', oldValue: data1[key], newValue: data2[key],
       };
     }
     return { key, type: 'unchanged', value: data1[key] };
   });
+  return diffTree;
 };
+
 export default buildTree;

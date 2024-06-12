e88f122c62e6b1323bb789434ec1db57bbaf24ec
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-12 12:19:14 +0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -9,14 +9,21 @@ const getExtFormat = (filepath) => path.extname(filepath).slice(1);
 
 const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
 
-const getData = (filepath) => parse(filepath, getExtFormat(filepath));
+const getData = (filepath) => {
+  const content = readFile(filepath);
+  const format = getExtFormat(filepath);
+  return parse(content, format);
+};
 
 const genDiff = (filepath1, filepath2) => {
-  const fullFilePath1 = readFile(getFullPath(filepath1));
-  const fullFilePath2 = readFile(getFullPath(filepath2));
+  const fullFilePath1 = getFullPath(filepath1);
+  const fullFilePath2 = getFullPath(filepath2);
 
   const data1 = getData(fullFilePath1);
   const data2 = getData(fullFilePath2);
-  return diffFormatter(buildTree(data1, data2));
+  const diffTree = buildTree(data1, data2);
+
+  return diffFormatter(diffTree);
 };
 export default genDiff;
+export { readFile };

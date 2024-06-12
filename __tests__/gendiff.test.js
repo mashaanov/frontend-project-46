99dfdbf258417c60a4b41309ecbd75fa313a8526
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-12 13:10:09 +0400

    update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,13 @@
-import genDiff from '../src/index.js';
-import { readFile } from '../src/index.js';
+import { genDiff, readFile } from '../src/index.js';
 import getFixturePath from '../src/helper.js';
 
 test('genDiff should return correct diff', () => {
-    const filePath1 = getFixturePath('file1.json');
-    const filePath2 = getFixturePath('file2.json');
-    const resultPath = getFixturePath('result_genDiff.txt');
+  const filePath1 = getFixturePath('file1.json');
+  const filePath2 = getFixturePath('file2.json');
+  const resultPath = getFixturePath('result_genDiff.txt');
 
-    const expectedResult = readFile(resultPath).trim(); // Удаление лишних пробелов и переносов строк
+  const expectedResult = readFile(resultPath).trim();
+  // Удаление лишних пробелов и переносов строк
 
-    expect(genDiff(filePath1, filePath2)).toEqual(expectedResult);
+  expect(genDiff(filePath1, filePath2)).toEqual(expectedResult);
 });

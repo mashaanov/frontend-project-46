63c21f1da1da46ffcc6a1d8a55d41d4b2249ae68
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-28 21:27:45 +0400

    update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -22,7 +22,7 @@ test('testing stylish JSON default', () => {
   expect(genDiff(filePath1, filePath2, 'stylish')).toEqual(expectedResult);
 });
 
-test('testing plain feature', () => {
+test('testing plain JSON', () => {
   const filePath1 = getFixturePath('file1.json');
   const filePath2 = getFixturePath('file2.json');
   const resultPath = getFixturePath('expectFilePlain.txt');
@@ -30,4 +30,25 @@ test('testing plain feature', () => {
   const expectedResult = readFile(resultPath).trim();
 
   expect(genDiff(filePath1, filePath2, 'plain')).toEqual(expectedResult);
+});
+
+test('testing plain YAML', () => {
+  const filePath1 = getFixturePath('file1.yaml');
+  const filePath2 = getFixturePath('file2.yaml');
+  const resultPath = getFixturePath('expectFilePlain.txt');
+
+  const expectedResult = readFile(resultPath).trim();
+
+  expect(genDiff(filePath1, filePath2, 'plain')).toEqual(expectedResult);
+});
+
+test('testing json formatting', () => {
+  const filePath1 = getFixturePath('file1.yaml');
+  const filePath2 = getFixturePath('file2.yaml');
+  const resultPath = getFixturePath('diffJSON.txt');
+
+  const expectedResult = readFile(resultPath).trim();
+  const expectedJsonString = JSON.stringify(JSON.parse(expectedResult));
+
+  expect(genDiff(filePath1, filePath2, 'json')).toEqual(expectedJsonString);
 });

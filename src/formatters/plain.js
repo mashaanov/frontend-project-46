1f9fd4b777a549144938f066575827ec38ffce41
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-28 20:34:36 +0400

    update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,39 +1,37 @@
 import _ from 'lodash';
 
 const getFormattedValue = (value) => {
-    if (_.isObject(value) && value !== null) {
-        return '[complex value]';
-    }
-    if (_.isString(value)) {
-        return `${value}`;
-    }
-    return value;
+  if (_.isObject(value) && value !== null) {
+    return '[complex value]';
+  }
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  return value;
 };
 
-const getPropertyPath = (parentPath, property) => {
-    return parentPath ? `${parentPath}${property}` : property;
-};
+const getPropertyPath = (parentPath, property) => (parentPath ? `${parentPath}.${property}` : property);
 
-const plainFormatter = (diffTree, parentPath = '') {
-    const lines = diffTree
+const plainFormatter = (diffTree, parentPath = '') => {
+  const lines = diffTree
     .map((node) => {
-        const propertyPath = getPropertyPath(parentPath, node.key);
-        switch (node.type) {
-            case 'added':
-                return `'${propertyPath}' was added with value: ${getFormattedValue(value)}`;
-            case 'deleted':
-                return `'${propertyPath}' was removed`;
-            case 'changed':
-                return `'${propertyPath}' was was updated. From ${node.oldValue} to ${node.newValue}`;
-            case 'nested':
-                return plainFormatter(node.children, propertyPath);
-            case 'unchanged':
-                return null;
-            default:
-                throw new Error(`Unknown node type: ${node.type}`);
-        }
+      const propertyPath = getPropertyPath(parentPath, node.key);
+      switch (node.type) {
+        case 'added':
+          return `Property '${propertyPath}' was added with value: ${getFormattedValue(node.value)}`;
+        case 'deleted':
+          return `Property '${propertyPath}' was removed`;
+        case 'changed':
+          return `Property '${propertyPath}' was updated. From ${getFormattedValue(node.oldValue)} to ${getFormattedValue(node.newValue)}`;
+        case 'nested':
+          return plainFormatter(node.children, propertyPath);
+        case 'unchanged':
+          return null;
+        default:
+          throw new Error(`Unknown node type: ${node.type}`);
+      }
     });
-    return lines.filter((line) => {line !== null}).join('\n');
+  return lines.filter((line) => line !== null).join('\n');
 };
 
-export default getFormattedValue;
+export default plainFormatter;

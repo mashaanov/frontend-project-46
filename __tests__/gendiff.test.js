bc547676ff9456f453f6a0a4dd6559944dce2fcf
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-28 14:23:07 +0400

    update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,7 +4,7 @@ import getFixturePath from '../src/helper.js';
 test('genDiff should return correct diff for JSON files', () => {
   const filePath1 = getFixturePath('file1.json');
   const filePath2 = getFixturePath('file2.json');
-  const resultPath = getFixturePath('result_genDiff.txt');
+  const resultPath = getFixturePath('expectFileStylish.txt');
 
   const expectedResult = readFile(resultPath).trim();
   // Удаление лишних пробелов и переносов строк
@@ -15,7 +15,7 @@ test('genDiff should return correct diff for JSON files', () => {
 test('genDiff should return correct diff for YAML files', () => {
   const filePath1 = getFixturePath('file1.yaml');
   const filePath2 = getFixturePath('file2.yaml');
-  const resultPath = getFixturePath('result_genDiffNested.txt');
+  const resultPath = getFixturePath('expectFileStylish.txt');
 
   const expectedResult = readFile(resultPath).trim();
   // Удаление лишних пробелов и переносов строк

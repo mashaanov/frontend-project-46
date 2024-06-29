1094cef5de8b217e7fcdc0ec92da524514b04437
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-06-29 19:22:51 +0400

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,11 +6,12 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
   .option('-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
-  .helpOption('-h, --help', 'output usage information')
-  .action((filepath1, filepath2) => {
-    genDiff(filepath1, filepath2);
+  .option('-f, --format <type>', 'output format: "stylish", "plain" or "json"', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
   });
+
 program.parse(process.argv);
